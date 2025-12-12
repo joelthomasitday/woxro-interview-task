6ab366134910f19cf559436c8ede4421a7a52cb9
wizdeveloper99 <wizdeveloper99@gmail.com>
2025-12-13 00:16:26 +0530

    Update RootLayout to suppress hydration warnings in layout.tsx for improved rendering consistency.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} antialiased`}>
         <Script
           src="https://unpkg.com/lenis@1.1.19/dist/lenis.min.js"
